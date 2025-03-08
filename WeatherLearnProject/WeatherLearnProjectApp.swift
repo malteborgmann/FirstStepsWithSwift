65fcfefe98eeb14d9421f514c7419a859c6a658e
Malte Borgmann <malte@borgmann.tech>
2025-03-08 16:29:10 +0300

    Initial Commit

---
+++ WeatherLearnProject/WeatherLearnProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherLearnProjectApp.swift
+//  WeatherLearnProject
+//
+//  Created by Malte Borgmann on 08.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherLearnProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherLearnProjectTests/WeatherLearnProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherLearnProjectTests.swift
+//  WeatherLearnProjectTests
+//
+//  Created by Malte Borgmann on 08.03.2025.
+//
+
+import Testing
+@testable import WeatherLearnProject
+
+struct WeatherLearnProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
